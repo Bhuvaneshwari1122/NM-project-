610349c10644a3bacefac3e62815d0efd1962684
Bhuvaneshwari1122 <148609475+Bhuvaneshwari1122@users.noreply.github.com>
2023-11-14 12:23:51 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,123 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+
+        ]
+    }, 
+     {
+        numb: 2,
+        question: "The correct sequence of HTML tags for starting a webpage is-",
+        answer: "D. HTML, Head, Title, Body",
+        options: [
+            "A. Head, Title, HTML, Body",
+            "B. HTML, Body, Head, Title",
+            "C. HTML, Title, Body, Head",
+            "D. HTML, Head, Title, Body"
+
+        ]
+    }, 
+     {
+        numb: 3,
+        question: "The <hr> tag in HTML is used for-",
+        answer: "D. horizontal ruler",
+        options: [
+            "A. new line",
+            "B. vertical ruler",
+            "C. new paragraph",
+            "D. horizontal ruler"
+
+        ]
+    }, 
+{
+    numb: 4,
+    question: "Which of the following is used to read an HTML page and render it?",
+    answer: "C. Web browser",
+    options: [
+        "A. Web server",
+        "B. Web network",
+        "C. Web browser",
+        "D. Web matrix"
+    ]
+}, 
+ {
+    numb: 5,
+    question: "Which element is used for or styling HTML5 layout?",
+    answer: "A. CSS",
+
+    options: [
+        "A. CSS",
+        "B. jQuery",
+        "C. JavaScript",
+        "D. PHP"
+
+    ]
+},
+{
+    numb: 6,
+    question: "The full form of CSS is:",
+    answer: "B. Cascading Style Sheets",
+
+    options: [
+        "A. Coloured Special Sheets",
+        "B. Cascading Style Sheets",
+        "C. Color and Style Sheets",
+        "D. None of the above",
+    ]
+},
+{
+    numb: 7,
+    question: "How can we change the background color of an element?",
+    answer: "A. background-color",
+
+    options: [
+        "A. background-color",
+        "B. color",
+        "C. Both A and B",
+        "D. None of the above",
+        ]
+},
+{
+    numb: 8,
+    question: "The CSS property used to specify the transperancy of an element is?",
+    answer: "C. opacity",
+
+    options: [
+        "A. visibility",
+        "B. filter",
+        "C. opacity",
+        "D. None of the above",
+    ]
+
+},
+{
+    numb: 9,
+    question: "How can we specify the spacing between each letter in a text in CSS?",
+    answer: "D. letter-spacing",
+    
+    options: [
+        "A. alpha-spacing",
+        "B. character-spacing",
+        "C.None of the above",
+        "D. letter-spacing",
+    ]
+},
+{
+    numb: 10,
+    question: "We can make rounded borders around elements using which CSS element?",
+    answer: "C. border-radius",
+    
+    options: [
+        "A. border-collapse",
+        "B. border-round",
+        "C. border-radius",
+        "D. none of the above",
+    ]
+},
+]; 
